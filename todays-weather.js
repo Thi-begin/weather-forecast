7f38b1c52952a54c7cebedf90865677e1fe5e4ff
Thi-begin <ihtihtt@gmail.com>
2026-04-05 20:59:21 +0700

    today's weather

---
+++ todays-weather.js
@@ -0,0 +1,12 @@
+let temperature = 85;
+let conditions =  "sunny";
+let sunriseTime = "7:00";
+let sunsetTime = "6:15";
+
+console.log(temperature)
+console.log(conditions)
+console.log(sunriseTime)
+console.log(sunsetTime)
+
+
+console.log(`It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} AM and will set at ${sunsetTime} PM. `);
