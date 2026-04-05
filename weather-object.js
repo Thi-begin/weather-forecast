1b790bbffb9050279af3cc6c7de8bc3aafce72c4
Thi-begin <ihtihtt@gmail.com>
2026-04-05 22:59:58 +0700

    weather object

---
+++ weather-object.js
@@ -0,0 +1,8 @@
+let weather = {
+    temperature:  75,
+    conditions: 'clear'
+}
+
+console.log(weather.temperature)
+
+console.log(`It's ${weather.temperature} degrees F and ${weather.conditions}. `)
